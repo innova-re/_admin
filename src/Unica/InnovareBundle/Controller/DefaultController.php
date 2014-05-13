2bd0d5857231b489179123a58f373a8162b93f9d
Antonio Pierro <antonio.pierro@gmail.com>
2014-05-13 15:26:57 +0200

    added the Unica/InnovareBundle by 'php app/console generate:bundle'

---
+++ src/Unica/InnovareBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Unica\InnovareBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('UnicaInnovareBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
